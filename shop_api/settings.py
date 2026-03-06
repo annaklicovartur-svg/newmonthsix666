23354fcc022ccf80c027c3199193e24ce690c45f
Artur <annaklicovartur@gmail.com>
2026-03-06 17:54:47 +0600

    Added hw3.6

--- shop_api/settings.py
+++ shop_api/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for production aa
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
